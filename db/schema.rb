4a3aba45c1ca60825f450e7681f91047620e9a5f
ehab93 <ehalbadawy93@gmail.com>
2015-02-03 22:06:01 +0200

    Create the reset of database relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203193333) do
+ActiveRecord::Schema.define(version: 20150203195908) do
+
+  create_table "choices", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "group_name"
@@ -26,6 +32,19 @@ ActiveRecord::Schema.define(version: 20150203193333) do
   end
 
   add_index "groups_users", ["group_id", "user_id"], name: "index_groups_users_on_group_id_and_user_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "right_answer"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "user_id"
