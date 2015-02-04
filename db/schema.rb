f3dbb3cc85efb08452aea3cbbcdb66ee818373b0
ehab93 <ehalbadawy93@gmail.com>
2015-02-04 13:43:26 +0200

    Add user_result to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203205709) do
+ActiveRecord::Schema.define(version: 20150204114137) do
 
   create_table "choices", force: :cascade do |t|
     t.string   "text"
@@ -52,8 +52,9 @@ ActiveRecord::Schema.define(version: 20150203205709) do
   create_table "users", force: :cascade do |t|
     t.string   "user_name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_result"
   end
 
 end
