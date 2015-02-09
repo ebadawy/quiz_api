f1ba5074f80df25bdd6e8644419193a0857ca2ca
ehab93 <ehalbadawy93@gmail.com>
2015-02-10 00:25:47 +0200

    Add user/quiz/question ids to answer's table

---
+++ db/migrate/20150209163621_add_missing_attr_to_answer_table.rb
@@ -0,0 +1,8 @@
+class AddMissingAttrToAnswerTable < ActiveRecord::Migration
+  def change
+  	add_column "answers", "user_id", :integer
+  	add_column "answers", "quiz_id", :integer
+  	add_column "answers", "question_id", :integer
+
+  end
+end
