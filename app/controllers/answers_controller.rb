f0999ec3821a532cc31df4db910d30c1207e91bc
ehab93 <ehalbadawy93@gmail.com>
2015-02-15 01:10:15 +0200

    Create a sperated create_answer method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -21,18 +21,21 @@ class AnswersController < ApplicationController
   # POST /answers.json
   def create
     passed = true
+    count = 0
     if params[:status] == "listOfAnswers"
       params[:answers].each do |ans|
         @answer = Answer.new(answers_params(ans))
         if @answer.save
           if @answer.correct
-            update_result
+            count = count + 1
+            # update_result ans[:user_id], ans[:quiz_id]
           end
         else
           passed = false
         end
       end
       if passed
+        create_result params[:answers].first[:user_id], params[:answers].first[:quiz_id], count
         render json: @answer, status: :created, location: @answer
       else
         render json: @answer.errors, status: :unprocessable_entity
@@ -72,14 +75,34 @@ class AnswersController < ApplicationController
 
   private
 
-    def update_result
-      currnet_quiz = Quize.find(params[:quiz_id]) 
+    def update_result user_id, quiz_id
+      puts "**********#{quiz_id}"
+      currnet_quiz = Quiz.find(quiz_id) 
       quiz_mark = currnet_quiz.quiz_mark
       question_no = currnet_quiz.questions.size
-      question_weight = quiz_mark / question_no
-      rslt = Result.where({user_id: params[:user_id], quiz_id: params[:quiz_id]}).first
+      if question_no != 0
+        question_weight = quiz_mark / question_no
+      else
+        question_weight = 0
+      end
+      rslt = Result.where({user_id: user_id, quiz_id: quiz_id}).first
+
       old_rslt = rslt.result
       rslt.result = old_rslt + questions_weight
+      rslt.save
+    end
+
+    def create_result user_id, quiz_id, count
+      currnet_quiz = Quiz.find(quiz_id) 
+      quiz_mark = currnet_quiz.quiz_mark
+      question_no = currnet_quiz.questions.size
+      if question_no != 0
+        question_weight = quiz_mark / question_no
+      else
+        question_weight = 0
+      end
+      rslt = Result.new({result: (count*question_weight),
+       user_id: user_id, quiz_id: quiz_id, published: false})
       rslt.save
     end
 
